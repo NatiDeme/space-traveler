c3410703deffd6f99fbdba46373596f35ec96279
HanningtoneM <hanningtonepompyy@gmail.com>
2022-07-19 16:36:30 +0300

    Add Reduxlogger

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import missions from './missions/missions';
+
+const rootReducer = combineReducers({
+  missions,
+});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
