87f585ad49bcc62751d6b1a52394c561bb1b3b6c
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-07-19 13:29:06 +0300

    Add nav header to codebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import MyProfile from './components/my-profile/MyProfile';
+import Nav from './components/NavBar/Nav';
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
       <Routes>
         <Route path="/" element={<Navigate replace to="/rockets" />} />
         <Route path="/profile" element={<MyProfile />} />
