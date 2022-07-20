3703bdec25d730ede1353858383ab7ad143c3bb4
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-07-20 11:52:39 +0300

    Add testing environment to rockets features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Nav from './components/NavBar/Nav';
 import MyProfile from './components/my-profile/MyProfile';
-
+import Rockets from './components/rockets/Rockets';
 import Missions from './components/missions/Missions';
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Navigate replace to="/rockets" />} />
+        <Route path="/rockets" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/profile" element={<MyProfile />} />
       </Routes>
